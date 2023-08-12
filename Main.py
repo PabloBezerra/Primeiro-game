4c66b6554fbdd7d1927b8484bdacdf0046bd1ac6
Pablo Bezerra <Pdsbezerra1316@gmail.com>
2023-08-12 02:58:37 -0300

    Continuando a construção do game

--- Main.py
+++ Main.py
@@ -3,11 +3,13 @@
 # IMPORTAÇÕES
 
 from PyQt6 import uic, QtWidgets, QtGui
-from random import randint
 
 # VARIÁVEIS E LISTAS
 
 contador = 0
+jogadas = 0
+placarX = 00
+placarO = 00
 dadosX = []
 dadosO = []
 
@@ -29,14 +31,15 @@ class Area():
         self.condicao = condicao
 
     def marcar(self):
-        global contador
+        global contador, jogadas
         self.marcado = 'O' if contador % 2 == 0 else 'X'
-
+        
         if self.condicao == True:
             exec(f"game.area{self.numero}.setPixmap(QtGui.QPixmap('{self.marcado}.png'))")
             self.condicao = False
             game.informacao.setText('')
             contador += 1
+            jogadas += 1
         else:
             game.informacao.setText('Casa já selecionada. Por favor escolha outra!')
 
@@ -47,15 +50,61 @@ class Area():
             dadosO.append(self.numero)
             dadosO.sort()
         verificador()
-
+        atualizador()
+    
 
 def verificador():
     if comp(dadosO, ltop) or comp(dadosO, lcenter) or comp(dadosO, ldwon) or comp(dadosO, cleft) or comp(dadosO, ccenter) or comp(dadosO, cright) or comp(dadosO, d1) or comp(dadosO, d2):
-        print('O venceu')
-    elif comp(dadosO, ltop) or comp(dadosO, lcenter) or comp(dadosO, ldwon) or comp(dadosO, cleft) or comp(dadosO, ccenter) or comp(dadosO, cright) or comp(dadosO, d1) or comp(dadosO, d2):
-        print('X venceu')
+        conslusao('O')
+    elif comp(dadosX, ltop) or comp(dadosX, lcenter) or comp(dadosX, ldwon) or comp(dadosX, cleft) or comp(dadosX, ccenter) or comp(dadosX, cright) or comp(dadosX, d1) or comp(dadosX, d2):
+        conslusao('X')
+    elif jogadas == 9:
+        conslusao()
+
+
+def atualizador():
+    if contador % 2 == 0:
+        game.informacao.setText('Turno do -> O')
+        game.placar_imgx.setPixmap(QtGui.QPixmap('X.png'))
+        game.placar_imgo.setPixmap(QtGui.QPixmap('O_over.png'))
     else:
-        print('Empate')
+        game.informacao.setText('Turno do -> X')
+        game.placar_imgx.setPixmap(QtGui.QPixmap('X_over.png'))
+        game.placar_imgo.setPixmap(QtGui.QPixmap('O.png'))
+
+
+def conslusao(m=''):
+    if m == 'X' or m == 'O':
+        resultado.show()
+        resultado.resultado.setText('VENCEU!')
+        resultado.logo.setPixmap(QtGui.QPixmap(f'{m}_over.png'))
+    else:
+        resultado.show()
+        resultado.resultado.setText('EMPATE!')
+        resultado.logo.setPixmap(QtGui.QPixmap('velha.png'))
+
+
+def play_novamente():
+    limpar()
+    resultado.close()
+
+
+def finalizar():
+    resultado.close()
+    game.close()
+    lobby.show()
+
+
+def limpar():
+    global dadosO, dadosX , jogadas
+    dadosO = []
+    dadosX = []
+    jogadas = 0
+
+    for n in range(1, 10):
+        exec(f'game.area{n}.setText(" ")')
+        exec(f'area{n}.condicao = True')
+        exec(f"area{n}.marcado = ''")
 
 
 def comp(li1, li2):
@@ -71,6 +120,10 @@ def comp(li1, li2):
 def load(n):
     lobby.close()
     game.show()
+    if jogadas != 0:
+        limpar()
+    game.placar_imgx.setPixmap(QtGui.QPixmap('X.png'))
+    game.placar_imgo.setPixmap(QtGui.QPixmap('O_over.png'))
 
 
 # INICIALIZAÇÃO DO GAME
@@ -78,15 +131,20 @@ def load(n):
 app = QtWidgets.QApplication([])
 lobby = uic.loadUi('Looby.ui')
 game = uic.loadUi('Game.ui')
+resultado = uic.loadUi('Resultado.ui')
 
 lobby.show()
 game.close()
+resultado.close()
 
 lobby.pessoaxrobo.clicked.connect(lambda: load(1))
 lobby.pessoacontra.clicked.connect(lambda: load(2))
 
 for n in range(1, 10):
-    exec(f'area{n} = Area({n})\n'
-        f'game.btn{n}.clicked.connect(lambda: area{n}.marcar())')
+    exec(f'area{n} = Area(n)')
+    exec(f'game.btn{n}.clicked.connect(lambda: area{n}.marcar())')
+
+resultado.jogar_novamente.clicked.connect(play_novamente)
+resultado.finalizar.clicked.connect(finalizar)
 
 app.exec()
